4e0353dc3cf515acb0914e2698e1ade190714f22
Nauval-Sutisna <nauval.master99@gmail.com>
2025-10-19 13:43:18 +0700

    Update dashboard.blade.php

--- resources/views/pasien/dashboard.blade.php
+++ resources/views/pasien/dashboard.blade.php
@@ -1,6 +1,3 @@
-<!-- <h1>Selamat Datang Pasien</h1>
-<form method="POST" action="/logout">@csrf <button>Logout</button></form> -->
-
-<x-layouts.app title="Pasien">
+<x-layouts.app title="Pasien Dashboard">
     <h1 class="ml-4">Halo Selamat Datang Pasien</h1>
 </x-layouts.app>
