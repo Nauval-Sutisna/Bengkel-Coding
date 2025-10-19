d1b93cabfebeb01f21b76b843e6986bb5d591dec
Nauval-Sutisna <nauval.master99@gmail.com>
2025-10-19 13:43:14 +0700

    Update dashboard.blade.php

--- resources/views/dokter/dashboard.blade.php
+++ resources/views/dokter/dashboard.blade.php
@@ -1,6 +1,3 @@
-<!-- <h1>Selamat Datang Dokter</h1>
-<form method="POST" action="/logout">@csrf <button>Logout</button></form> -->
-
-<x-layouts.app>
-    <h1 class="ml-4 mt-2">Selamat Datang Di Halaman Dokter</h1>
+<x-layouts.app title="Dokter Dashboard">
+    <h1 class="ml-4">Halo Selamat Datang Dokter</h1>
 </x-layouts.app>
