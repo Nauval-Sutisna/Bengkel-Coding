7ff6bb404092052595bd41d90f08f52c17fa478d
Nauval-Sutisna <nauval.master99@gmail.com>
2025-10-19 13:42:41 +0700

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,44 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use App\Model\User;
-
-
-
 class AuthController extends Controller
 {
-    public function showLogin(){
+    public function showLogin()
+    {
         return view('auth.login');
     }
-
-    public function login(Request $request){
+    public function login(Request $request)
+    {
         $credentials = $request->only('email', 'password');
 
-        if(auth::attempt($credentials)){
+        if (Auth::attempt($credentials)) {
             $user = Auth::user();
-            if($user->role == 'admin'){
-                return redirect()->route('admin.dasboard');
-            }elseif($user->role == 'dokter'){
+
+            if ($user->role == 'admin') {
+                return redirect()->route('admin.dashboard');
+            } elseif ($user->role == 'dokter') {
                 return redirect()->route('dokter.dashboard');
-            }else{
+            } else {
                 return redirect()->route('pasien.dashboard');
             }
         }
+
         return back()->withErrors(['email' => 'Email atau Password Salah!']);
     }
 
-    public function register(Request $request){
+    public function register(Request $request)
+    {
         $request->validate([
             'nama' => ['required', 'string', 'max:255'],
             'alamat' => ['required', 'string', 'max:255'],
-            'no.ktp' => ['required', 'string', 'max:30'],
+            'no_ktp' => ['required', 'string', 'max:30'],
             'no_hp' => ['required', 'string', 'max:20'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email'],
             'password' => ['required', 'confirmed'],
         ]);
+
 
         User::create([
             'nama' => $request->nama,
@@ -47,9 +49,16 @@ class AuthController extends Controller
             'no_ktp' => $request->no_ktp,
             'no_hp' => $request->no_hp,
             'email' => $request->email,
-            'password' => $request->Hash::make($request->password),
+            'password' => Hash::make($request->password),
             'role' => 'pasien',
         ]);
+
+        return redirect()->route('login');
+    }
+
+    public function logout()
+    {
+        Auth::logout();
         return redirect()->route('login');
     }
 }
