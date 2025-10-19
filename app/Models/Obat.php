b66ae550a11f711a2c82a35680111401555fdd70
Nauval-Sutisna <nauval.master99@gmail.com>
2025-10-19 13:42:51 +0700

    Update Obat.php

--- app/Models/Obat.php
+++ app/Models/Obat.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Obat extends Model
 {
     protected $table = 'obat';
-
+    
     protected $fillable = [
         'nama_obat',
         'kemasan',
@@ -15,7 +15,6 @@ class Obat extends Model
     ];
 
     public function detailPeriksa(){
-        return $this->hasMany(detailPeriksa::class, 'id_obat');
+        return $this->hasMany(User::class, 'id_obat');
     }
 }
-// 
