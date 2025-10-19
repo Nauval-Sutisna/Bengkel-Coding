d4458a07e9fcd26abc58805dbe937627c279e77c
Nauval-Sutisna <nauval.master99@gmail.com>
2025-10-19 13:43:19 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,33 @@
 <?php
 
-// use Illuminate\Support\Facades\Route;
-// use App;
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
-Route::get('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login']);
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
-Route::get('/register', [AuthController::class, 'register']);
-Route::get('/logout', [AuthController::class, 'logout']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout']);
 
-// Admin
-Route::middleware(['auth', 'role:admin'])->prefix('admin')->group(function (){
-    Route::get('/dashboard', function(){
+//Admin
+Route::middleware(['auth', 'role:admin'])->prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
         return view('admin.dashboard');
     })->name('admin.dashboard');
 });
 
-// Dokter
+//Dokter
 Route::middleware(['auth', 'role:dokter'])->prefix('dokter')->group(function () {
     Route::get('/dashboard', function () {
         return view('dokter.dashboard');
     })->name('dokter.dashboard');
 });
 
-// Pasien
+//Pasien
 Route::middleware(['auth', 'role:pasien'])->prefix('pasien')->group(function () {
     Route::get('/dashboard', function () {
         return view('pasien.dashboard');
